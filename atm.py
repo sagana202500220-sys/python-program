537db4ebf7e3bda31abc6a9d370eae5811fc9fad
sagana202500220-sys <sagana202500220@gmail.com>
2025-07-31 15:32:24 +0530

    Create atm.py

---
+++ atm.py
@@ -0,0 +1,7 @@
+pin =input("What you want:")
+if pin=='withdraw':
+    print("take a amount")
+    amount=int(input("How much money you want:"))
+    print("take a cash",amount)
+else:
+    print("deposit your money")
