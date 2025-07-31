9e88269eb2a7b2bc907749bab612d6745e3e49e4
sagana202500220-sys <sagana202500220@gmail.com>
2025-07-31 15:20:52 +0530

    Create even.py

---
+++ even.py
@@ -0,0 +1,9 @@
+                                                           odd or even algorithm
+1 start
+2 input a number
+3 check if num%2==0
+4 if true,then:
+5 print"Even number"
+6 else:
+7 print"odd number"
+8 end
