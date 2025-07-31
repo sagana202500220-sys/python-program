84e1682ccd1163b0335cc1184ec690915daadff7
sagana202500220-sys <sagana202500220@gmail.com>
2025-07-31 15:20:05 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,8 @@
+                                      prime numbers.....
+1 start
+2 declare prime numbers
+3 find variables
+4 print("Enter a prime numbers")
+5 else :
+6 print("prime numbers")
+7 end
